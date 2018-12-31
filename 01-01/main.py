fca551ed1688d2986a0dd6104467bd1cabd81b73
Ulf Krantz <u@nulf.se>
2018-12-31 12:34:37 +0100

    Remove static input of variables

--- 01-01/main.py
+++ 01-01/main.py
@@ -2,13 +2,11 @@
 # Solution for advent of code 2018 01/12 puzzle 1
 #
 
-# Get the three frequencies from user, as  integers
-frequency_1 = int(input("Frequency #1 : "))
-frequency_2 = int(input("Frequency #2 : "))
-frequency_3 = int(input("Frequency #3 : "))
+# Read the input file as a list
 
-# Calculate the resulting frequency
-resulting_frequency = (frequency_1 + frequency_2 + frequency_3)
+
+# Loop through all values in the list, if it's a positive value add it, otherwise subtract it
+
 
 # Output resulting frequency
-print("The resulting frequency is : {}".format(resulting_frequency))
+print("The resulting frequency is : {}".format(calculated_frequency))
