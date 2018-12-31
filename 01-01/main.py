4dc88f60130c7ee8609e27c80e39988b3af338da
Ulf Krantz <u@nulf.se>
2018-12-31 13:07:56 +0100

    Add reading of input frequencies and loop through to get calculated frequency

--- 01-01/main.py
+++ 01-01/main.py
@@ -3,9 +3,15 @@
 #
 
 # Read the input file as a list
+input_file = open('input.txt', 'r')
+input_frequencies = input_file.read().splitlines()
+input_file.close()
 
 
 # Loop through all values in the list, if it's a positive value add it, otherwise subtract it
+calculated_frequency = 0
+for input_frequency in input_frequencies:
+    calculated_frequency = eval('calculated_frequency + int(input_frequency)')
 
 
 # Output resulting frequency
