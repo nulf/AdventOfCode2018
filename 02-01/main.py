2abca42761f99c510b4ebbb2dd338b112ce3a730
Ulf Krantz <u@nulf.se>
2019-01-04 19:56:51 +0100

    First try att puzzle

--- 02-01/main.py
+++ 02-01/main.py
@@ -1,10 +1,33 @@
 #
 # Solution for advent of code 2018 02/12 part 1
+#
 
 
-# Read the input file as a list
+# Function for reading the input file as a list
 def read_input_file():
     input_file = open('input.txt', 'r')
     input_box_ids = input_file.read().splitlines()
     input_file.close()
     return input_box_ids
+
+
+# Read input file with box ID's
+box_ids = read_input_file()
+
+# Integer that counts if there are two of the same character in box_id
+pairs = 0
+
+# Integer that counts if there are two of the same character in box_id
+three_of_a_kind = 0
+
+# How many of the current character we can find in box_id
+# Loop through all box ID's
+for box_id in box_ids:
+    for character in box_id:
+        amount_of_that_character = box_id.count('characters')
+        if amount_of_that_character == 2:
+            pairs += 1
+        if amount_of_that_character == 3:
+            three_of_a_kind += 1
+
+print('Total sum : {}'.format(pairs * three_of_a_kind))
