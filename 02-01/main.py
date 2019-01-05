c429572ce4d3d101d2db9f5da5e3bc94c5bc5ae2
Ulf Krantz <u@nulf.se>
2019-01-05 09:45:12 +0100

    Solution for part 1

--- 02-01/main.py
+++ 02-01/main.py
@@ -14,20 +14,31 @@ def read_input_file():
 # Read input file with box ID's
 box_ids = read_input_file()
 
-# Integer that counts if there are two of the same character in box_id
+# Integer that counts if there are exactly two of the same character in box_id
 pairs = 0
+pairs_in_loop = 0
 
-# Integer that counts if there are two of the same character in box_id
+# Integer that counts if there are exactly three of the same character in box_id
 three_of_a_kind = 0
+three_of_a_kind_in_loop = 0
 
-# How many of the current character we can find in box_id
 # Loop through all box ID's
 for box_id in box_ids:
     for character in box_id:
-        amount_of_that_character = box_id.count('characters')
+
+        # Holds the amount of characters of a certain type in box_id
+        amount_of_that_character = box_id.count(character)
         if amount_of_that_character == 2:
-            pairs += 1
+            pairs_in_loop += 1
         if amount_of_that_character == 3:
-            three_of_a_kind += 1
+            three_of_a_kind_in_loop += 1
+
+    # If we found pairs or three of a kind, add one to the counter and reset in loop counter
+    if pairs_in_loop > 0:
+        pairs_in_loop = 0
+        pairs += 1
+    if three_of_a_kind_in_loop > 0:
+        three_of_a_kind_in_loop = 0
+        three_of_a_kind += 1
 
 print('Total sum : {}'.format(pairs * three_of_a_kind))
